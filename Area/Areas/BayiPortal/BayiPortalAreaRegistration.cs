587617890f3b3e100852ad5affb9851e077ae133
Gokhan Irmak <gokhanirmak2@hotmail.com>
2018-02-08 15:05:03 +0300

    Add files via upload

---
+++ Area/Areas/BayiPortal/BayiPortalAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Area.Areas.BayiPortal
+{
+    public class BayiPortalAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "BayiPortal";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "BayiPortal_default",
+                "BayiPortal/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Area/Areas/BayiPortal/Controllers/PortalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Area.Areas.BayiPortal.Controllers
+{
+    public class PortalController : Controller
+    {
+        // GET: BayiPortal/Portal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
